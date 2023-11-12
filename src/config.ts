6ab0dbe16e22c4ab98c6b0179488960697629868
Boldizsar Mezei <boldizsar@unchained.work>
2023-11-12 14:30:36 +0100

    Clean master

--- src/config.ts
+++
@@ -1,6 +0,0 @@
-export const OUTPUT_FILE = "./build5.zip";
-
-export const BUILD_5_TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOiIweDU1MWZkMmM3YzdiZjM1NmJhYzE5NDU4N2RhYjJmY2Q0NjQyMDA1NGIiLCJwcm9qZWN0IjoiMHg0NjIyM2VkZDQxNTc2MzVkZmM2Mzk5MTU1NjA5ZjMwMWRlY2JmZDg4IiwiaWF0IjoxNjk1ODUyNTk2fQ.WT9L4H9eDdFfJZMrfxTKhEq4PojNWSGNv_CbmlG9sJg";
-
-export const BUILD5_URL = "https://api-test.build5.com/uploadfile";
